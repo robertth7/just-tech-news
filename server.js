8638b0cc43777c3cc88ea1839a0d0b6208e3bc91
Roberto Hurtado Jr <rhurtado.267@gmail.com>
2023-11-14 15:54:46 -0600

    pluralized, date opt, and url formatted

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const sequelize = require('./config/connection');
 // require css path
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
